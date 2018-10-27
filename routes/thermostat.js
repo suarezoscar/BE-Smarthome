25250e42b2dd3b572c84f1c09cbcae2623357d1e
oscar <oscar-su@live.fr>
2018-10-27 13:13:35 +0200

    fixed routes

--- routes/thermostat.js
+++ routes/thermostat.js
@@ -3,15 +3,15 @@ const router = new Router();
 const Ctrl = require('../controllers/thermostat');
 
 //Get methods
-router.get('/', Ctrl.getThermostat);
-router.get('/current', Ctrl.getCurrentTemp);
-router.get('/status', Ctrl.getCurrentStatus);
-router.get('/trigger',Ctrl.getTriggerTemp);
+router.get('', Ctrl.getThermostat);
+router.get('current', Ctrl.getCurrentTemp);
+router.get('status', Ctrl.getCurrentStatus);
+router.get('trigger',Ctrl.getTriggerTemp);
 
 //Put Methods
-router.put('/current/:temp', Ctrl.setCurrentTemp);
-router.put('/status/:status', Ctrl.setStatus);
-router.put('/trigger/:temp', Ctrl.setTriggerTemp);
+router.put('current/:temp', Ctrl.setCurrentTemp);
+router.put('status/:status', Ctrl.setStatus);
+router.put('trigger/:temp', Ctrl.setTriggerTemp);
 
 
 module.exports = router.routes();
